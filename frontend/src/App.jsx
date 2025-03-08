e1651dd717305dbd83eca4ea46aadb9822c713ce
nagesh-merva <officialnagesh.merva@gmail.com>
2025-03-08 12:34:23 +0530

    inital setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom"
+
+
+function App() {
+
+  return (
+    <div className="md:flex justify-center items-center h-screen w-full">
+      <Routes>
+        <Route index path="/" element={<></>} />
+        <Route index path="/events" element={<></>} />
+        <Route index path="/results" element={<></>} />
+        <Route index path="/aboutus" element={<></>} />
+        <Route index path="/gallery" element={<></>} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
