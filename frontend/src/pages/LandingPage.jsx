e923934d4ae57d066aa595de6b906a1fec340eee
nagesh-merva <officialnagesh.merva@gmail.com>
2025-03-14 02:08:42 +0530

    ADDING SCROLL TOP

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -3,8 +3,16 @@ import Aboutus from "../sections/Aboutus"
 import HeroSection from "../sections/HeroSection"
 import Sponsors from "../sections/Sponsors"
 import Footer from "../sections/Footer"
+import ScrollToTop from "@/components/ScrollToTop"
+import { useEffect } from "react"
 
 export default function LandingPage() {
+    const { pathname } = useLocation()
+
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [pathname])
+
     return (
         <div className="h-full w-full bg-black space-y-10 sm:space-y-16 md:space-y-24"
             style={{
@@ -13,6 +21,7 @@ export default function LandingPage() {
                 `,
                 backgroundSize: "20px 20px",
             }}>
+            <ScrollToTop />
             <HeroSection />
             <Aboutus />
             <PrizePool />
--- frontend/src/pages/ResultsPage.jsx
+++ frontend/src/pages/ResultsPage.jsx
@@ -1,9 +1,14 @@
 import ResultsHero from "@/sections/results/ResultsHero"
 import Winners from "@/sections/results/Winners"
 import Footer from "@/sections/Footer"
-import RetroNavbar from "@/components/RetroNavbar"
+import { useEffect } from "react"
 
 export default function ResultsPage() {
+    const { pathname } = useLocation()
+
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [pathname])
 
     return (
         <div className="w-full h-full ">
