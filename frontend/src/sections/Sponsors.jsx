e9d5a0f3c1563da86b48d56a747cc07ffbaa9fbd
amithkumar10 <amith10mnr@gmail.com>
2025-03-17 19:20:39 +0530

    Added new sponsors

--- frontend/src/sections/Sponsors.jsx
+++ frontend/src/sections/Sponsors.jsx
@@ -36,6 +36,46 @@ const Sponsors = () => {
               className="object-contain w-44 h-44"
             />
           </div>
+
+          {/* Sponsor 4 */}
+          <div className="flex justify-center">
+            <img
+              src="/sponsors/Veera.jpeg"
+              alt="Veera logo"
+              className="object-contain w-44 h-44"
+            />
+          </div>
+
+          {/* Sponsor 5 */}
+          <div className="flex justify-center">
+            <img
+              src="/sponsors/Ocean.png"
+              alt="Ocean logo"
+              className="object-contain w-44 h-44"
+            />
+          </div>
+
+          {/* Sponsor 5 */}
+          <div className="flex justify-center">
+            <div className="bg-gradient-to-br from-gray-200 to-gray-400 w-48 h-48 rounded-full flex flex-col items-center justify-center shadow-lg">
+              <p className="text-sm font-medium text-gray-800 text-center">
+                Best wishes from
+              </p>
+              <p className="text-lg font-bold text-gray-900 text-center">
+                Sameer Mandrekar (G.D)
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Special Thanks Section */}
+        <div className="mt-20 text-center">
+          <h2 className="text-5xl font-bold text-creamy">Special Thanks</h2>
+          <div className="mt-10 ">
+            <p className="text-creamy text-4xl sm:text-sm md:text-4xl font-medium">
+              Mr. Ranjan Uttam Shirodkar, Taleigao
+            </p>
+          </div>
         </div>
       </div>
     </div>
